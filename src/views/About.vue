3d4eab54c2585b9559a98a88a9abce645e2ebcd3
Putnam3145 <putnam3145@gmail.com>
2022-04-02 14:01:32 -0700

    Rewrote e-begging to reflect shifting values

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,7 +3,7 @@
     <b-row>
       <b-col>
     Hi, I'm Putnam. I've got 8 years of programming under my belt, all of it for open source projects. I'm not exactly known, but whenever I show up to a community that needs programmers, I've historically turned heads: people take notice of me and my skill.<br><br>
-    All this bragaddocio is to say that, actually, by the software industry's reckoning, I have literally 0 experience and no degree. I have never been paid money for my programming work, which makes all of it objectively valueless, if you consider the only objective measurement of value to be payment. In order to rectify this, I would greatly appreciate a bit of money sent my way, so as to show the industry that maybe my skills are worth being paid for. Also, because I have to eat.
+    All this bragaddocio is to say that, since I live in the middle of nowhere and have no experience I've actually been <i>paid</i> for, all of this experience means nothing, as I have no meaningful connections to prove to anyone that I have experience with value. So, e-begging is my only real means for income. Whoops!
       </b-col>
       <b-col>
     <iframe src='https://ko-fi.com/putnam3145/?hidefeed=true&widget=true&embed=true&preview=true' style='border:none;width:min-content;padding:4px;background:#f9f9f9;' height='712' title='putnam3145'></iframe>
