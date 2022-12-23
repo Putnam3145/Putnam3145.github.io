3e42685d48f341ea68578eadfb836ce024aaf505
Putnam3145 <putnam3145@gmail.com>
2022-12-22 18:18:03 -0800

    Quick removal of e-begging

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,11 +3,11 @@
     <b-row>
       <b-col>
     Hi, I'm Putnam. I've got 8 years of programming under my belt, all of it for open source projects. I'm not exactly known, but whenever I show up to a community that needs programmers, I've historically turned heads: people take notice of me and my skill.<br><br>
-    All this bragaddocio is to say that, since I live in the middle of nowhere and have no experience I've actually been <i>paid</i> for, all of this experience means nothing, as I have no meaningful connections to prove to anyone that I have experience with value. So, e-begging is my only real means for income. Whoops!
+    As of January 2023, I am an independent contractor working on <b-link href="http://www.bay12games.com/dwarves/">Dwarf Fortress</b-link>
       </b-col>
-      <b-col>
+      <!--<b-col>
     <iframe src='https://ko-fi.com/putnam3145/?hidefeed=true&widget=true&embed=true&preview=true' style='border:none;width:min-content;padding:4px;background:#f9f9f9;' height='712' title='putnam3145'></iframe>
-      </b-col>
+      </b-col>-->
     </b-row>
   </b-container>
 </template>
